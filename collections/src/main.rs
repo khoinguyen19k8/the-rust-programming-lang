ddf5780675ea13e7f2fb025682f76055c4e28f27
khoinguyen19k8 <khoinguyen19k8@gmail.com>
2025-01-09 00:30:34 +0100

    Add initial code for parse_config of minigrep

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    // Vector
+
+    // let v_empty: Vec<i32> = Vec::new();
+    let v = !vec![1, 2, 3, 4, 5];
+    let mut v_mutable: Vec<i32> = Vec::new();
+    v_mutable.push(5);
+    v_mutable.push(6);
+    v_mutable.push(7);
+    v_mutable.push(8);
+
+    // Strings
+
+    // Hash Maps
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,7 +1,20 @@
-// use std::env;
+use std::env;
+use std::fs;
 
 fn main() {
-//     let args: Vec<String> = env::args().collect();
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let query: &String = &args[1];
+    let file_path: &String = &args[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("File content:\n{contents}")
+}
+
+fn parse_config(args: &Vec<String>) -> (&str, &str) {
+
 }
 
